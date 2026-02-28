09cac8a213da2bbe7877b26842f1344364b69638
Satvik <b.satvik99@gmail.com>
2026-02-28 16:42:26 +0530

    Initial Commit

---
+++ VirtualTrainer/VirtualTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualTrainerApp.swift
+//  VirtualTrainer
+//
+//  Created by Satvik Bansal on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct VirtualTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
